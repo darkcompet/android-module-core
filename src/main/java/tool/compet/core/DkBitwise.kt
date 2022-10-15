7d2cffe741e236831bc031222b3aa14810b62557
DarkCompet <darkcompet@gmail.com>
2022-10-15 10:57:12 +0700

    Add comment

--- src/main/java/tool/compet/core/DkBitwise.kt
+++ src/main/java/tool/compet/core/DkBitwise.kt
@@ -4,6 +4,8 @@ class DkBitwise {
 	companion object {
 		/**
 		 * Check whether bit at given index is 1.
+		 *
+		 * @param index Start from right (end) to left (start), from 0-index.
 		 */
 		fun hasBitAt(value: Int, index: Int): Boolean {
 			return ((value shr index) and 1) == 1
