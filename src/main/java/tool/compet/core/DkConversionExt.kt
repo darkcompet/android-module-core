5b26952718af186f62250ff34cf884bf60bd193a
DarkCompet <darkcompet@gmail.com>
2022-10-27 00:27:58 +0700

    Fix bug parse int, long for floating point string

--- src/main/java/tool/compet/core/DkConversionExt.kt
+++ src/main/java/tool/compet/core/DkConversionExt.kt
@@ -1,29 +1,27 @@
 package tool.compet.core
 
-import java.lang.Exception
-
 /**
  * Provides conversion between all objects, for eg,. Primitives, Pixel, Dp, Sp,...
  * Functions inside this are safe and faster than Framework.
  */
 
-fun Boolean?.parseIntDk() : Int {
+fun Boolean?.parseIntDk(): Int {
 	return if (this == true) 1 else 0
 }
 
-fun Boolean.parseIntDk() : Int {
+fun Boolean.parseIntDk(): Int {
 	return if (this) 1 else 0
 }
 
-fun Int?.parseBooleanDk() : Boolean {
+fun Int?.parseBooleanDk(): Boolean {
 	return this != null && this != 0
 }
 
-fun String?.parseBooleanDk() : Boolean {
+fun String?.parseBooleanDk(): Boolean {
 	return "1" == this || "true".equals(this, ignoreCase = true)
 }
 
-fun String?.parseIntDk() : Int {
+fun String?.parseIntDk(): Int {
 	if (this == null) {
 		return 0
 	}
@@ -49,11 +47,14 @@ fun String?.parseIntDk() : Int {
 		if (ch in '0'..'9') {
 			result = (result shl 3) + (result shl 1) + (ch - '0') // 10x + d
 		}
+		else {
+			break
+		}
 	}
 	return if (minus) -result else result
 }
 
-fun String?.parseLongDk() : Long {
+fun String?.parseLongDk(): Long {
 	if (this == null) {
 		return 0
 	}
@@ -79,11 +80,14 @@ fun String?.parseLongDk() : Long {
 		if (ch in '0'..'9') {
 			result = (result shl 3) + (result shl 1) + (ch - '0') // 10x + d
 		}
+		else {
+			break
+		}
 	}
 	return if (minus) -result else result
 }
 
-fun String?.parseFloatDk() : Float {
+fun String?.parseFloatDk(): Float {
 	return try {
 		if (this == null) 0f else java.lang.Float.parseFloat(this)
 	}
@@ -92,7 +96,7 @@ fun String?.parseFloatDk() : Float {
 	}
 }
 
-fun String?.parseDoubleDk() : Double {
+fun String?.parseDoubleDk(): Double {
 	return try {
 		if (this == null) 0.0 else java.lang.Double.parseDouble(this)
 	}
