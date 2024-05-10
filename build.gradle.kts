7223ab8ea1197ee0a744378382739cb0f1d74179
DarkCompet <darkcompet@gmail.com>
2024-05-11 04:55:13 +0700

    Change to gradle kts

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+	alias(libs.plugins.android.library)
+	alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+	namespace = "tool.compet.core"
+
+	compileSdk = 34
+
+	defaultConfig {
+		minSdk = 21
+		targetSdk = 34
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+		consumerProguardFiles("consumer-rules.pro")
+	}
+
+	buildTypes {
+		// At debug mode, don't minify with R8, so quicker-build/lower-run.
+		debug {
+			isMinifyEnabled = false
+			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+		}
+
+		// At release mode, minify with R8, so lower-build/faster-run.
+		release {
+			isMinifyEnabled = true
+			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+		}
+	}
+
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_17
+		targetCompatibility = JavaVersion.VERSION_17
+	}
+
+	kotlinOptions {
+		jvmTarget = JavaVersion.VERSION_17.toString()
+	}
+
+	buildFeatures {
+		buildConfig = true
+	}
+}
+
+dependencies {
+	testImplementation(libs.junit)
+	androidTestImplementation(libs.androidx.junit)
+	androidTestImplementation(libs.androidx.espresso.core)
+
+	api(libs.androidx.core.ktx)
+	api(libs.androidx.appcompat)
+	api(libs.material)
+}
